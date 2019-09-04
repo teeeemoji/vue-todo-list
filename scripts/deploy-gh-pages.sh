5516c6bfd5177bb3467c3c4c62a5d9cadb8d045d
teeeemoji <teeeemoji@163.com>
2019-09-04 17:53:00 +0800

    ci: test deploy gh-pages shell script - not using run function

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -2,10 +2,13 @@
 source ./scripts/initial-ci.sh
 
 # delete all files except /dist
-run "find . -depth 1 -not -name 'dist' -not -name 'README.md' -delete"
+# run "find . -depth 1 -not -name 'dist' -not -name 'README.md' -delete"
+find . -depth 1 -not -name 'dist' -not -name 'README.md' -delete
 
 # move files in /dist folder to root folder
-run "mv /dist/* ./"
+# run "mv /dist/* ./"
+mv /dist/* ./
 
 # remove /dist folder
-run "rm -rf dist"
+# run "rm -rf dist"
+rm -rf dist
