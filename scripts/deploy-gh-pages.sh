28109c36c7afa24bcaee5a83222cd76b3eed1a47
teeeemoji <teeeemoji@163.com>
2019-09-04 17:39:28 +0800

    ci: fix deploy gh-pages shell script

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -2,7 +2,7 @@
 source ./scripts/initial-ci.sh
 
 # delete all files except /dist
-run "rm -rf !(/dist)"
+run "find . -d 1 -not -name dist -not -name README.md -delete"
 
 # move files in /dist folder to root folder
 run "mv /dist/* ./"
